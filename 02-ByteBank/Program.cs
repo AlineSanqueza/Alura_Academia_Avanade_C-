155ffa7af6b7e9df8c1cf5ad4e5293ef3bb3d2c0
Aline <alinesanqueza@gmail.com>
2020-10-06 11:24:29 -0300

    Adicionar arquivos de projeto.

---
+++ 02-ByteBank/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _02_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+            conta.saldo = 200;
+            Console.WriteLine(conta.saldo);
+
+            conta.saldo += 100;
+            Console.WriteLine(conta.saldo);
+
+            ContaCorrente outraConta = new ContaCorrente();
+            outraConta.saldo = 50;
+            Console.WriteLine("A primeira conta tem " + conta.saldo);
+            Console.WriteLine("A outra conta tem " + outraConta.saldo);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 05-ByteBank/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _05_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cliente gabriela = new Cliente();
+
+            //gabriela.nome = "Gabriela";
+           //gabriela.profissao = "Dev C#";
+          //gabriela.cpf = "123.456.789-12";
+
+            ContaCorrente conta = new ContaCorrente();
+
+            // conta.titular = gabriela;
+            //conta.titular = new Cliente();
+            //conta.titular.nome = "Gabriela Costa";
+            //conta.titular.cpf = "123.456.789-12";
+            //conta.titular.profissao = "Dev C#";
+
+            conta.saldo = 500;
+            conta.agencia = 876;
+            conta.numero = 345678;
+
+            if (conta.titular == null)
+            {
+                Console.WriteLine("A referência é null");
+            }
+
+            //Console.WriteLine(gabriela.nome);
+            Console.WriteLine(conta.titular);
+            //Console.WriteLine(conta.titular.nome);
+            //Console.WriteLine(conta.titular.cpf);
+            //Console.WriteLine(conta.titular.profissao);
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
